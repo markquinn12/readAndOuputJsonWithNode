4ea763a12f9e0e16fddcc9f43e01bc729e693977
Mark Quinn <markquinn12@gmail.com>
2015-12-04 21:53:04 +0000

    - more changes for NPM installation

--- dataConverter.js
+++ dataConverter.js
@@ -1,37 +1,41 @@
+module.exports = convert;
 var fs = require("fs");
-var tableName = process.argv.slice(2);
+convert();
 
-if(tableName == null || tableName == '')
-{
-      tableName = "DEFAULT_TABLE"  
-      console.log('Table name not passed as argument to script. Will insert DEFAULT_TABLE into script'); 
-}
-else
-{
-      console.log('Table name: ' +tableName ); 
-}
 
-var exportedData = require('./input.json');
-var items = exportedData.items;
-var entireOutput = '';
+function convert () {
+      var tableName = process.argv.slice(2);
 
-for (var itemKey in items) {
-      var stringToOutput = '<';
-      stringToOutput += tableName;
-
-      var object = items[itemKey];
-      for (var objectKey in object) {
-            stringToOutput += ' ';
-            stringToOutput += objectKey.toUpperCase();
-            stringToOutput += '=';
-            stringToOutput += '"' + object[objectKey] + '"';
+      if (tableName == null || tableName == '') {
+            tableName = "DEFAULT_TABLE"
+            console.log('Table name not passed as argument to script. Will insert DEFAULT_TABLE into script');
+      }
+      else {
+            console.log('Table name: ' + tableName);
       }
 
-      stringToOutput += "/>\n";
-      entireOutput += stringToOutput;
-}
+      var exportedData = require('./input.json');
+      var items = exportedData.items;
+      var entireOutput = '';
 
-fs.writeFile('output.txt', entireOutput, function (err) {
-  if (err) throw err;
-  console.log('File created: output.txt');
-});
+      for (var itemKey in items) {
+            var stringToOutput = '<';
+            stringToOutput += tableName;
+
+            var object = items[itemKey];
+            for (var objectKey in object) {
+                  stringToOutput += ' ';
+                  stringToOutput += objectKey.toUpperCase();
+                  stringToOutput += '=';
+                  stringToOutput += '"' + object[objectKey] + '"';
+            }
+
+            stringToOutput += "/>\n";
+            entireOutput += stringToOutput;
+      }
+
+      fs.writeFile('output.txt', entireOutput, function (err) {
+            if (err) throw err;
+            console.log('File created: output.txt');
+      });
+};
